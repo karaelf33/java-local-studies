c9fd78427eec2a0832399b1947772fd2e10096aa
besta <bestasyusuf8@gmail.com>
2021-05-14 21:07:03 +0300

    algorithm

---
+++ src/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReverseString {
+
+    public static String firstWayReverseStringMethod(String str) {
+        return new StringBuilder(str).reverse().toString();
+
+    }
+
+    public static String secondWayReverseStringMethod(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            stringBuilder.append(str.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstWayReverseStringMethod("yusufbestas"));
+        System.out.println("--------------------------");
+        System.out.println(secondWayReverseStringMethod("yusufbestas"));
+    }
+}
