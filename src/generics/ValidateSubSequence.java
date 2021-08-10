a403d731e17299dc679120cd32f461d298951385
besta <bestasyusuf8@gmail.com>
2021-08-10 22:38:12 +0300

    sorted and squared array

--- src/generics/ValidateSubSequence.java
+++ src/generics/ValidateSubSequence.java
@@ -19,7 +19,6 @@ public class ValidateSubSequence {
         return seqIndex == sequence.size();
 
     }
-
     public static void main(String[] args) {
 
         List<Integer> array = new ArrayList<>();
