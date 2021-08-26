7f1be5181f7abcda6a6d3be4b9a98ad30bf887cf
besta <bestasyusuf8@gmail.com>
2021-08-26 19:56:25 +0300

    hackerrank algorithm questions and answers

--- src/generics/Test.java
+++ src/generics/Test.java
@@ -6,8 +6,13 @@ import java.util.function.Supplier;
 
 public class Test {
 
+
     public static void main(String[] args) {
-        Collection<String> collection= Arrays.asList("a","b","c","d");
+        Collection<String> collection = Arrays.asList("a", "b", "c", "d");
+        String witch = "b";
+        String tail = "lack";
+        witch = witch.concat(tail);
+        System.out.println(witch);
 
     }
 
